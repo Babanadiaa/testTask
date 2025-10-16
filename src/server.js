196d91c71b1bb53629afa2643612438a227f5d1a
Maksym Atamanchuk <maksumatmn@gmail.com>
2025-10-16 15:09:38 +0300

    fix server

--- src/server.js
+++ src/server.js
@@ -4,6 +4,10 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
-server.listen(3000, () => {
-    console.log('JSON Server is running');
+
+// використовуємо порт від Render або 3000 для локально
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`JSON Server is running on port ${PORT}`);
 });
